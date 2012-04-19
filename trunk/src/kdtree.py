4942433d405ef5a3b81d1e9c73c3ad5ce2429fbe
mail@pedromoreira.org <mail@pedromoreira.org@73c755d7-abb4-d376-64d9-0ee45ea73145>
2012-04-19 01:27:30 +0000

    próximo passo implementar o heapsort que vai ser a primeira operação a fazer na lista para, posteriormente, se poder ordenar com o quicksort, efectuar pesquisas ou outras operações

--- trunk/src/kdtree.py
+++ trunk/src/kdtree.py
@@ -10,6 +10,8 @@
 #   - remoção de elementos;
 #   - pesquisa do elemento mais próximo;
 
+import random
+
 class kdtree:
     '''
     o que a classe faz...
@@ -21,15 +23,81 @@ class kdtree:
         uma lista de dados e ordená-la
         '''
         self.__A = A
-        self.__sort__()
+        self.__balance__()
         pass
 
-    def __sort__(self):
+    #################################################
+    # ORDENAR A LISTA
+    
+    def printSorted(self):
         '''
-        ordenar a lista, escolhendo o método apropriado
+        obter cópia da lista ordenada
         '''
+        #utilizando quicksort
+        B = self.__A
+        self.__randomizedQuicksort__(B, 0, len(self.__A) - 1)
+        print B
         pass
+    
+    #################################################
+    # livro pag 177
+    # RANDOMIZED PARTITION
 
+    def __randomizedPartition__(self, A, p, r):
+        i = random.randint(p,r)
+        A[r], A[i] = A[i], A[r]
+        return self.__partition__(A, p, r)
+    # FIM DO RANDOMIZED PARTITION
+    #################################################
+
+
+    #################################################
+    # livro pag 169
+    # PARTITION
+
+    def __partition__(self, A, p, r):
+        x = A[r]
+        i = p - 1
+        for j in range(p, r):
+            if A[j] <= x:
+                i += 1
+                A[i], A[j] = A[j], A[i]
+                pass
+            pass
+        A[i+1], A[r] = A[r], A[i+1]
+        return i+1
+    # FIM DO PARTITION
+    #################################################
+
+    
+    #################################################
+    # livro pag 177
+    # RANDOMIZED QUICKSORT
+
+    def __randomizedQuicksort__(self, A, p, r):
+        if p < r:
+            q = self.__randomizedPartition__(A, p, r)
+            self.__randomizedQuicksort__(A, p, q - 1)
+            self.__randomizedQuicksort__(A, q + 1, r)
+            pass
+        pass
+    # FIM DO RANDOMIZED QUICKSORT
+    #################################################
+
+
+    #################################################
+    # Balancear árvore
+
+    def __balance__(self):
+        """
+        Balancear a árvore com o heap sort
+        para optimizar as operações
+        a efectuar com a lista
+        """    
+        pass
+    # FIM DO BALANCEAR ÁRVORE
+    #################################################
+    
     def merge(self, A, B):
         '''
         fazer a junção de duas listas
@@ -55,9 +123,3 @@ class kdtree:
         pass
     
     pass
-
-def main():
-    
-    pass
-
-main()
